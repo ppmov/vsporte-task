1f0116079fb215f29e00c887512f6e0d3ac66675
Mikhail Yakimov <pupimov@gmail.com>
2023-01-17 01:28:52 +0500

    Update GameEventService.cs

--- task-2/Services/GameEventService.cs
+++ task-2/Services/GameEventService.cs
@@ -10,23 +10,4 @@ public class GameEventService : ServiceBase<GameEvent>
 
     protected override DbSet<GameEvent> DataTable => _context.GameEvents;
     public override string NavigationPropertyPath => string.Empty;
-
-    public Club? GetClub(Guid id) => _context.Clubs.SingleOrDefault(x => x.Id == id);
-    public Player? GetPlayer(Guid id) => _context.Players.SingleOrDefault(x => x.Id == id);
-
-    public override void Create(GameEvent item)
-    {
-        item.Club = GetClub(item.ClubId);
-        item.Player = item.PlayerId is null ? null : GetPlayer(item.PlayerId ?? Guid.Empty);
-        
-        base.Create(item);
-    }
-
-    public override void Update(GameEvent item)
-    {
-        item.Club = GetClub(item.ClubId);
-        item.Player = item.PlayerId is null ? null : GetPlayer(item.PlayerId ?? Guid.Empty);
-        
-        base.Update(item);
-    }
 }
